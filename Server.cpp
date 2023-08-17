bc38b230dbfe8997003bbcc3cf4a0f13c1b8770a
Nilay Majorwar <nilaymajorwar@gmail.com>
2023-08-18 00:11:14 +0530

    Move to epoll

--- Server.cpp
+++ Server.cpp
@@ -1,5 +1,4 @@
 #include <netinet/in.h>
-#include <poll.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string>
@@ -7,14 +6,20 @@
 #include <cstring>
 #include "Server.h"
 
-void crash(const std::string& base) {
+[[noreturn]] void crash(const std::string& base) {
     std::cerr << base << ": " << strerror(errno) << std::endl;
     std::exit(1);
 }
 
-Server::Server() = default;
+Server::Server() {
+    this->epoll_fd = epoll_create(1);
+    if (this->epoll_fd < 0) crash("epoll_create()");
+};
 
-Server::~Server() = default;
+Server::~Server() {
+    if (close(this->epoll_fd))
+        std::cout << "failed to close epoll fd: " << strerror(errno) << std::endl;
+};
 
 void Server::setup_listening_fd() {
     std::cout << "Setting up listening fd... ";
@@ -38,48 +43,57 @@ void Server::setup_listening_fd() {
 
     Server::fd_set_nonblock(fd);
 
+    struct epoll_event ee = {.events = EPOLLIN, .data = {.fd = fd}};
+    if (epoll_ctl(this->epoll_fd, EPOLL_CTL_ADD, fd, &ee))
+        crash("epoll_ctl(): failed for listen fd");
+
     this->listen_fd = fd;
     std::cout << "\x1b[32m success\x1b[0m" << std::endl;
 }
 
 void Server::start_event_loop() {
-    std::vector<struct pollfd> poll_args;
     std::cout << "\x1b[34mStarted event loop!\x1b[0m" << std::endl;
 
     while (true) {
-        poll_args.clear();
-        poll_args.push_back({this->listen_fd, POLLIN, 0});
-
+        // Update subscribed events for all existing connections
         for (Connection* conn : fd2conn) {
             if (!conn) continue;
-            struct pollfd pfd = {conn->fd};
-            pfd.events = (conn->state == STATE_WRITE) ? POLLOUT : POLLIN;
-            pfd.events = pfd.events | POLLERR;
-            poll_args.push_back(pfd);
+            struct epoll_event ee = {.events = 0, .data = {.fd = conn->fd}};
+            ee.events = (conn->state == STATE_WRITE) ? EPOLLOUT : EPOLLIN;
+            ee.events = ee.events | EPOLLERR;
+            if (epoll_ctl(this->epoll_fd, EPOLL_CTL_MOD, conn->fd, &ee)) {
+                printf("%d: epoll_ctl: mod: %s\n", conn->fd, strerror(errno));
+                conn->state = STATE_END;
+            };
         }
 
-        int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), 1000);
-        if (rv < 0) crash("poll()");
+        // Wait for events on current connections
+        struct epoll_event ready_events[16];
+        int ready_count = epoll_wait(this->epoll_fd, ready_events, sizeof(ready_events), 5000);
+        if (ready_count < 0) {
+            printf("epoll_wait(): %s\n", strerror(errno));
+            continue;
+        };
 
-        for (size_t i = 1; i < poll_args.size(); ++i) {
-            if (!poll_args[i].revents) continue;
-            Connection* conn = this->fd2conn[poll_args[i].fd];
-            if (conn->state == STATE_END) {
-                fd2conn[conn->fd] = nullptr;
-                close(conn->fd);
-                free(conn);
-            } else conn->process();
+        // Process ready connections
+        for (size_t i = 0; i < ready_count; ++i) {
+            if (ready_events[i].data.fd != this->listen_fd) {
+                Connection *conn = this->fd2conn[ready_events[i].data.fd];
+                if (conn->state == STATE_END) this->close_client_connection(conn);
+                else conn->process();
+            } else accept_client_connection();
         }
-
-        if (poll_args[0].revents) accept_client_connection();
     }
 }
 
-void Server::fd_set_nonblock(int fd) {
+bool Server::fd_set_nonblock(int fd) {
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags < 0) crash("fcntl(): get");
     flags |= O_NONBLOCK;
-    if (fcntl(fd, F_SETFL, flags) < 0) crash("fcntl(): set");
+    if (fcntl(fd, F_SETFL, flags) == -1) {
+        printf("%d: fcntl: %s\n", fd, strerror(errno));
+        return false;
+    } else return true;
 }
 
 void Server::accept_client_connection() {
@@ -88,10 +102,27 @@ void Server::accept_client_connection() {
     socklen_t socklen = sizeof(client_addr);
     int client_fd = accept(this->listen_fd, (struct sockaddr*)&client_addr, &socklen);
     if (client_fd < 0) crash("accept()");
-    Server::fd_set_nonblock(client_fd);
+    if (!Server::fd_set_nonblock(client_fd)) {
+        close(client_fd);
+        return;
+    };
 
     // Create Connection instance and insert into conn vector
     auto conn = new Connection(client_fd);
     if (this->fd2conn.size() <= client_fd) this->fd2conn.resize(client_fd + 1);
     this->fd2conn[client_fd] = conn;
+
+    struct epoll_event ee = {.events = 0, .data = {.fd = conn->fd}};
+    if (epoll_ctl(this->epoll_fd, EPOLL_CTL_ADD, client_fd, &ee)) {
+        printf("%d: epoll_ctl: add: %s\n", client_fd, strerror(errno));
+        conn->state = STATE_END;
+    };
+}
+
+void Server::close_client_connection(Connection* conn) {
+    if (epoll_ctl(this->epoll_fd, EPOLL_CTL_DEL, conn->fd, nullptr))
+        printf("%d: epoll_ctl[del]: %s\n", conn->fd, strerror(errno));
+    fd2conn[conn->fd] = nullptr;
+    close(conn->fd);
+    free(conn);
 }
--- Server.h
+++ Server.h
@@ -3,21 +3,24 @@
 
 
 #include <vector>
+#include <sys/epoll.h>
 #include "Connection.h"
 
 class Server {
 public:
-    explicit Server();
+    Server();
     ~Server();
     void setup_listening_fd();
     [[noreturn]] void start_event_loop();
 
 private:
     int listen_fd = -1;
+    int epoll_fd = -1;
     std::vector<Connection*> fd2conn;
 
-    static void fd_set_nonblock(int fd);
+    static bool fd_set_nonblock(int fd);
     void accept_client_connection();
+    void close_client_connection(Connection *conn);
 };
 
 
